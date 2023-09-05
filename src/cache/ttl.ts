f4545470bd8e94d53a97e10b65ed28fcef75c98d
hollyyy08 <hollyhanasakii@gmail.com>
2023-09-04 22:40:45 -0400

    added ttl.ts into the folder

--- src/cache/ttl.ts
+++ src/cache/ttl.ts
@@ -1,54 +1,35 @@
 'use strict';
+
 module.exports = function (opts) {
-    const LRU = require('lru-cache');
+    const TTLCache = require('@isaacs/ttlcache');
     const pubsub = require('../pubsub');
-    // lru-cache@7 deprecations
-    const winston = require('winston');
-    const chalk = require('chalk');
-    // sometimes we kept passing in `length` with no corresponding `maxSize`.
-    // This is now enforced in v7; drop superfluous property
-    if (opts.hasOwnProperty('length') && !opts.hasOwnProperty('maxSize')) {
-        winston.warn(`[cache/init(${opts.name})] ${chalk.white.bgRed.bold('DEPRECATION')} ${chalk.yellow('length')} was passed in without a corresponding ${chalk.yellow('maxSize')}. Both are now required as of lru-cache@7.0.0.`);
-        delete opts.length;
-    }
-    const deprecations = new Map([
-        ['stale', 'allowStale'],
-        ['maxAge', 'ttl'],
-        ['length', 'sizeCalculation'],
-    ]);
-    deprecations.forEach((newProp, oldProp) => {
-        if (opts.hasOwnProperty(oldProp) && !opts.hasOwnProperty(newProp)) {
-            winston.warn(`[cache/init(${opts.name})] ${chalk.white.bgRed.bold('DEPRECATION')} The option ${chalk.yellow(oldProp)} has been deprecated as of lru-cache@7.0.0. Please change this to ${chalk.yellow(newProp)} instead.`);
-            opts[newProp] = opts[oldProp];
-            delete opts[oldProp];
-        }
-    });
-    const lruCache = new LRU(opts);
+
+    const ttlCache = new TTLCache(opts);
+
     const cache = {};
     cache.name = opts.name;
     cache.hits = 0;
     cache.misses = 0;
     cache.enabled = opts.hasOwnProperty('enabled') ? opts.enabled : true;
-    const cacheSet = lruCache.set;
-    // expose properties while keeping backwards compatibility
+    const cacheSet = ttlCache.set;
+
+    // expose properties
     const propertyMap = new Map([
-        ['length', 'calculatedSize'],
-        ['calculatedSize', 'calculatedSize'],
         ['max', 'max'],
-        ['maxSize', 'maxSize'],
         ['itemCount', 'size'],
         ['size', 'size'],
         ['ttl', 'ttl'],
     ]);
-    propertyMap.forEach((lruProp, cacheProp) => {
+    propertyMap.forEach((ttlProp, cacheProp) => {
         Object.defineProperty(cache, cacheProp, {
             get: function () {
-                return lruCache[lruProp];
+                return ttlCache[ttlProp];
             },
             configurable: true,
             enumerable: true,
         });
     });
+
     cache.set = function (key, value, ttl) {
         if (!cache.enabled) {
             return;
@@ -57,47 +38,53 @@ module.exports = function (opts) {
         if (ttl) {
             opts.ttl = ttl;
         }
-        cacheSet.apply(lruCache, [key, value, opts]);
+        cacheSet.apply(ttlCache, [key, value, opts]);
     };
+
     cache.get = function (key) {
         if (!cache.enabled) {
             return undefined;
         }
-        const data = lruCache.get(key);
+        const data = ttlCache.get(key);
         if (data === undefined) {
             cache.misses += 1;
-        }
-        else {
+        } else {
             cache.hits += 1;
         }
         return data;
     };
+
     cache.del = function (keys) {
         if (!Array.isArray(keys)) {
             keys = [keys];
         }
-        pubsub.publish(`${cache.name}:lruCache:del`, keys);
-        keys.forEach(key => lruCache.delete(key));
+        pubsub.publish(`${cache.name}:ttlCache:del`, keys);
+        keys.forEach(key => ttlCache.delete(key));
     };
     cache.delete = cache.del;
+
     cache.reset = function () {
-        pubsub.publish(`${cache.name}:lruCache:reset`);
+        pubsub.publish(`${cache.name}:ttlCache:reset`);
         localReset();
     };
     cache.clear = cache.reset;
+
     function localReset() {
-        lruCache.clear();
+        ttlCache.clear();
         cache.hits = 0;
         cache.misses = 0;
     }
-    pubsub.on(`${cache.name}:lruCache:reset`, () => {
+
+    pubsub.on(`${cache.name}:ttlCache:reset`, () => {
         localReset();
     });
-    pubsub.on(`${cache.name}:lruCache:del`, (keys) => {
+
+    pubsub.on(`${cache.name}:ttlCache:del`, (keys) => {
         if (Array.isArray(keys)) {
-            keys.forEach(key => lruCache.delete(key));
+            keys.forEach(key => ttlCache.delete(key));
         }
     });
+
     cache.getUnCachedKeys = function (keys, cachedData) {
         if (!cache.enabled) {
             return keys;
@@ -112,17 +99,21 @@ module.exports = function (opts) {
             }
             return !isCached;
         });
+
         const hits = keys.length - unCachedKeys.length;
         const misses = keys.length - hits;
         cache.hits += hits;
         cache.misses += misses;
         return unCachedKeys;
     };
+
     cache.dump = function () {
-        return lruCache.dump();
+        return Array.from(ttlCache.entries());
     };
+
     cache.peek = function (key) {
-        return lruCache.peek(key);
+        return ttlCache.get(key, { updateAgeOnGet: false });
     };
+
     return cache;
 };
